30f53665e1ad60778fee93c5d22d9656b3491c2d
FilipM <filip.moszczynski@onet.pl>
2020-12-19 14:21:24 +0100

    initial commit

---
+++ src/main/java/pl/rental/FilBudRentExPolApplication.java
@@ -0,0 +1,13 @@
+package pl.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilBudRentExPolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilBudRentExPolApplication.class, args);
+	}
+
+}
