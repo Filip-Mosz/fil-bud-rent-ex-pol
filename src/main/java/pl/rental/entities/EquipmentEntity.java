fb56f2ff00e7dc85fafe486c5dbcca07d755b83c
FilipM <filip.moszczynski@onet.pl>
2020-12-21 15:10:34 +0100

    EquipmentEntity

---
+++ src/main/java/pl/rental/entities/EquipmentEntity.java
@@ -0,0 +1,30 @@
+package pl.rental.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Repository
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EquipmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String brand;
+    String model;
+    String type;
+    String status;
+    Long prizeForDay;
+
+}
