992522880dfee4e9e2baa7b3a4eda3bd2c077142
FilipM <filip.moszczynski@onet.pl>
2020-12-28 13:15:21 +0100

    Rent+return - Dto, Entity and Repository + setting new names for tables

---
+++ src/main/java/pl/rental/dtos/RentDto.java
@@ -0,0 +1,14 @@
+package pl.rental.dtos;
+
+import java.sql.Date;
+
+public class RentDto {
+
+    private Long id;
+    private Long machineId;
+    private Long clientId;
+    private Long employeeId;
+    private Date dateOfRent;
+    private Date estimatedDateOfReturn;
+
+}
---
+++ src/main/java/pl/rental/dtos/ReturnDto.java
@@ -0,0 +1,14 @@
+package pl.rental.dtos;
+
+import java.sql.Date;
+
+public class ReturnDto {
+
+    private Long id;
+    private Long machineId;
+    private Long clientId;
+    private Long employeeId;
+    private Date dateOfReturn;
+    private Long delayInDays;
+
+}
